2fadb02e5b8b29afdd9a634db8df3ecaf521d0d3
Adrian Ross <adriantoddross@gmail.com>
2020-02-22 18:36:28 -0500

    add selectBox function

--- game-of-life/src/components/Box.js
+++ game-of-life/src/components/Box.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 export function Box(props) {
+  const selectBox = () => {
+    props.selectBox(props.row, props.col)
+  };
+
   return (
     <div
       className={props.boxClass}
       id={props.id}
-      onClick={props.selectBox}
-    ></div>
+      onClick={selectBox}
+    />
   );
 }
