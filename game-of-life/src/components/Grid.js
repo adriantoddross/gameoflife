b380a04ce228766125c264a48809a64393eba534
Adrian Ross <adriantoddross@gmail.com>
2020-02-22 18:34:24 -0500

    import box

--- game-of-life/src/components/Grid.js
+++ game-of-life/src/components/Grid.js
@@ -1,5 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
+import { Box } from './Box';
 
-export function Grid() {
-  return <div>Grid</div>;
+export function Grid(props) {
+  const width = props.cols * 14;
+  let rowsArr = [];
+  let boxClass = '';
+  
+  for (let i = 0; i < props.rows; i++) {
+        for (let j = 0; j < props.cols; j++) {
+      let boxId = i + '_' + j;
+      boxClass = props.gridFull[i][j] ? 'box on' : 'box off';
+      rowsArr.push(
+        <Box
+          boxClass={boxClass}
+          key={boxId}
+          boxId={boxId}
+          row={i}
+          col={j}
+          selectBox={props.selectBox}
+        />
+      )
+    }
+    
+  }
+
+  return <div className='grid' style={{width: width}}>
+  {{rowsArr}}
+ </div>;
 }
