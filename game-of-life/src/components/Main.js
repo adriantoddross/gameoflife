8ef69b56fb3a0af095d877d8ed97be8f0a167285
Adrian Ross <adriantoddross@gmail.com>
2020-02-22 13:15:00 -0500

    create gridFull and pass props to Grid component

--- game-of-life/src/components/Main.js
+++ game-of-life/src/components/Main.js
@@ -2,11 +2,25 @@ import React, { useState } from "react";
 import { Grid } from "./Grid";
 
 export function Main() {
+  let speed = 100;
+  let rows = 30;
+  let cols = 50;
+
   const [generation] = useState(0);
+  const [gridFull] = useState(
+    Array(rows)
+      .fill()
+      .map(() => Array(cols).fill(false))
+  );
+
   return (
     <div>
       <h1>The Game of Life</h1>
-      <Grid />
+      <Grid 
+        gridFull={gridFull}
+        rows={rows}
+        cols={cols}
+      />
       <h2>Generations: {generation}</h2>
     </div>
   );
